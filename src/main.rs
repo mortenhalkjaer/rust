33d8fbe753e1be1c0d04f6cb88f57a7e57e1f8fe
Morten <mh@ivt2.com>
2022-12-28 16:41:36 +0100

    First version

--- src/main.rs
+++ src/main.rs
@@ -31,3 +31,8 @@ fn handle_connection(mut stream: TcpStream) {
   let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
   stream.write_all(response.as_bytes()).unwrap();
 }
+
+/*
+  Rust-analyzer
+  CodeLLDB
+*/
