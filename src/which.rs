35a4dde61cccfbd7ba18b715157c16a65eb350a0
Morten <mh@ivt2.com>
2023-01-03 16:51:53 +0100

    Which

--- src/which.rs
+++ src/which.rs
@@ -1,32 +1,41 @@
 use std::env;
 use std::fs;
+use std::path;
 
 pub mod which {
     use std::ascii::AsciiExt;
 
   fn search(directory: &str, search_for: &String) -> Option<String> {
-    for file in std::fs::read_dir(directory).unwrap() {
-      let filename = file.unwrap().file_name().into_string().unwrap();
-      if filename.len() > 3 {
-        let ext = String::from(&filename[filename.len()-3..]).to_ascii_lowercase();
-        let name = &filename[0..filename.len()-4];
+    let b : bool = std::path::Path::new(directory).is_dir();
 
-        if (("exe" == ext) || ( "bat" == ext)) && search_for.eq_ignore_ascii_case(name) {
-          return Some(filename)
+    // todo : skip emtpy paths
+
+    if b {
+      for file in std::fs::read_dir(directory).unwrap() {
+        let filename = file.unwrap().file_name().into_string().unwrap();
+        if filename.len() > 3 {
+          let ext = String::from(&filename[filename.len()-3..]).to_ascii_lowercase();
+          let name = &filename[0..filename.len()-4];
+
+          if (("exe" == ext) || ( "bat" == ext)) && search_for.eq_ignore_ascii_case(name) {
+            return Some(filename)
+          }
         }
       }
+    } else {
+      println!("Invalid directory {}\n", directory);
     }
     None
   }
 
   pub fn run() {
-    // todo : error handling - if empty/missing path
-    // todo : push current directory into this one
     let path = std::env::var("PATH").unwrap();
     let filename: String = String::from("fcw");
+    // todo : get filename from command line
+    // todo: avoid duplicated code
+    search(".\\", &filename).and_then(|f|{ println!("{}\\{}", ".\\", f); Some(f) } );
 
     for dir in path.split(";") {
-      // todo : path - with directory not found
       search(dir, &filename).and_then(|f|{ println!("{}\\{}", dir, f); Some(f) } );
     }
   }
