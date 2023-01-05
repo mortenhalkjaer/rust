2f0617131f999e3cacdfcdb24804fcdf04c36ca2
Morten <mh@ivt2.com>
2023-01-05 11:08:18 +0100

    chained vector

--- src/which.rs
+++ src/which.rs
@@ -3,10 +3,12 @@ pub mod which {
     use std::env;
 //    use std::fs;
     use std::path;
-    
+    use std::str;
+    //use std::iter;
+
   fn starts_with(s: &String, substr: &str) -> bool {
     if substr.len() > s.len() { return false; }
-    
+
     for (sub, main) in substr.chars().zip(s.chars()) {
       if !sub.eq_ignore_ascii_case(&main) {
         return false;
@@ -25,7 +27,7 @@ pub mod which {
 
   fn search(directory: &str, search_for: &String, extensions: &Vec<&str>) -> Option<String> {
     let b : bool = path::Path::new(directory).is_dir() && directory.len()>0;
-    
+
     if b {
       for file in std::fs::read_dir(directory).unwrap() {
         let filename = file.unwrap().file_name().into_string().unwrap();
@@ -36,7 +38,7 @@ pub mod which {
             if filename.len() == s.len() + search_for.len() {
               if starts_with(&filename, search_for) {
 
-              
+
                 if ends_with(&filename, s) {
                   return Some(filename);
                 }
@@ -57,14 +59,14 @@ pub mod which {
     // todo : get filename from command line
     // todo: avoid duplicated code - use chain
 
+    let current : Vec<&str> = vec!(".\\");
+    let paths : Vec<&str> = path.split(";").collect();
+
     let extensions: Vec<&str> = pathext.split(";").collect();
 
-    search(".\\", &filename, &extensions)
-      .and_then(|f|{ println!("Found: {}{}", ".\\", f); Some(f) } );
-
-    for dir in path.split(";") {
+    for dir in current.iter().chain(paths.iter()) {
       search(dir, &filename, &extensions)
-        .and_then(|f|{ println!("Found: {}{}", dir, f); Some(f) } );
+      .and_then(|f|{ println!("Found: {}{}", dir, f); Some(f) } );
     }
   }
 }
