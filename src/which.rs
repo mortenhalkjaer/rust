da21b463e27deb2357009f380c148c02806acd86
unknown <mortenhalkjaer@gmail.com>
2023-01-06 17:15:18 +0100

    clena up

--- src/which.rs
+++ src/which.rs
@@ -1,57 +1,51 @@
-
 pub mod which {
-    use std::env;
-    use std::fs;
-    use std::io;
-  
-    use std::str;
-    use std::iter;
-    
-    //use std::iter;
+    use std::{env, fs, io, str, iter};
 
-  fn starts_with(s: &String, substr: &str) -> bool {
-    if substr.len() > s.len() { return false; }
-
-    for (sub, main) in substr.chars().zip(s.chars()) {
-      if !sub.eq_ignore_ascii_case(&main) {
-        return false;
-      }
-    }
-    return true;
+    // todo  :make as extensions to String
+  pub fn starts_with(s: &String, substr: &str) -> bool {        
+    substr.chars().zip(s.chars())
+      .all(|(sub,main)| sub.eq_ignore_ascii_case(&main))
   }
 
   fn ends_with(s: &String, substr: &str) -> bool {
-    if substr.len() > s.len() { return false; }
-    for (sub, main) in substr.chars().rev().zip(s.chars().rev()) {
-      if !sub.eq_ignore_ascii_case(&main) { return false; }
-    }
-    return true;
+    substr.chars().rev().zip(s.chars().rev())
+      .all(|(sub, main)| sub.eq_ignore_ascii_case(&main))
   }
 
-  fn search(directory: &str, search_for: &String, extensions: &Vec<&str>) -> io::Result<Option<String>> {
-    let s =
-    fs::read_dir(directory)?.into_iter()
-      .filter_map(|file|{
-        let filename = file.unwrap().file_name().into_string().unwrap();
-        if filename.len() >= search_for.len() { return Some(filename); } else { return None; }
-      })
-      .find(|filename| {
-        let expected_length = filename.len() - search_for.len();
-
-        extensions.into_iter()
-            .filter(|ext| { ext.len() == expected_length })
+  fn search(directory: &str, search_for: &String, extensions: &Vec<&str>) -> io::Result<Option<String>> {    
+    Ok(fs::read_dir(directory)?.into_iter()
+      .map(|file| file.unwrap().file_name().into_string().unwrap())
+      .find(|filename| 
+        extensions.into_iter()           
             .any(|ext| { starts_with(&filename, search_for) && ends_with(&filename, ext) })
-      });
-
-    return Ok(s);
+      ))
   }
 
   pub fn run() {    
-    let path = env::var("PATH").expect("Missing PATH");
-    let pathext = env::var("PATHEXT").expect("Missing PATHEXT");
+    let path = env::var("PATH").expect("Missing PATH");    
     let filename: String = String::from("xcopy");
-    // todo : get filename from command line
+    // todo : get filename from command line     
+    let pathext = env::var("PATHEXT").expect("Missing PATHEXT");
     let extensions: Vec<&str> = pathext.split(";").collect();
+    
+
+
+
+/* 
+    find way to replace result check and option handling
+
+    for dir in iter::once(".\\").chain(path.split(";")) {
+      search(dir, &filename, &extensions).expect(msg)
+        Ok(filename) => {
+          match filename {
+            Some(f) => println!("Found: {}\\{}", dir, f),
+            None => {}
+          }
+        },
+        Err(err) => { println!("Unable to scan {}: {} ", dir, err); }
+      };
+    }; 
+*/
  
     for dir in iter::once(".\\").chain(path.split(";")) {
       match search(dir, &filename, &extensions) {
@@ -63,6 +57,8 @@ pub mod which {
         },
         Err(err) => { println!("Unable to scan {}: {} ", dir, err); }
       };
-    };
+    }; 
+    
+
   }
 }
